6d3b8f4a31b597ea9ec5e2629a2cf16a6547b2c2
Timo Stollenwerk <stollenwerk@kitconcept.com>
2025-06-22 17:19:05 +0200

    Fix indentation and Python style for example 12.

--- examples/12_speed_fan.py
+++ examples/12_speed_fan.py
@@ -1,7 +1,8 @@
-import spidev
-import time
 import sys
+import time
+
 import RPi.GPIO as GPIO
+import spidev
 
 GPIO.setmode(GPIO.BCM)
 motor_pin = 12
@@ -11,23 +12,26 @@ spi = spidev.SpiDev()
 spi.open(0,0)
 spi.max_speed_hz = 1000000
 
-pwm = GPIO.PWM(motor_pin,80)
+pwm = GPIO.PWM(motor_pin, 80)
 pwm.start(0)
 
+
 def readadc(adcnum):
-	r = spi.xfer2([1,8+adcnum<<4,0])
-	adcout = ((r[1]&3)<<8)+r[2]
-	return adcout
+    r = spi.xfer2([1, 8 + adcnum << 4, 0])
+    adcout = ((r[1] & 3) << 8)+r[2]
+    return adcout
+
 
 def brightness_func(x):
-        return x/10.23
+    return x/10.23
+
 
 try:
-        while True:
-            value = readadc(0)
-            brightness = brightness_func(value)
-            pwm.ChangeDutyCycle(brightness)
-            time.sleep(0.1)
+    while True:
+        value = readadc(0)
+        brightness = brightness_func(value)
+        pwm.ChangeDutyCycle(brightness)
+        time.sleep(0.1)
 
 except KeyboardInterrupt:
-        GPIO.cleanup()
+    GPIO.cleanup()
